aecdea342ddca36339b1af2aeee2016000bf9bb2
sindikoi <sindikoifman@gmail.com>
2025-06-08 12:44:03 +0300

    server

--- server/server.js
+++ server/server.js
@@ -6,15 +6,20 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
-app.use(cors());
-app.use(express.json());
-
 //connect to mongoDB , the user are for the mongo will work well
 // takes the data uri from the .env if he can connect 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log("connected to MongoDB"))
-.catch((err) => console.log(err));
+.then(() => { console.log("connected to MongoDB")
+
+  //middleware
+   app.use(cors());
+   app.use(express.json());
+
+//the check of the user coonects our router to the server
+  app.use('/api/auth', require('./routes/auth'));
+
+})
+.catch ((err) => console.log(err));
